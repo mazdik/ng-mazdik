32846a7578e540737e1976a0a5c48a578a481ee8
User <remappp@gmail.com>
2017-02-25 13:30:43 +0500

    headers

--- header/header.component.ts
+++ header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 
 @Component({
     selector: '[datatable-header]',
@@ -13,6 +13,16 @@ export class HeaderComponent {
 
     public sortField: string;
     public sortOrder: number;
+
+    @ViewChild('selectFilter') selectFilter: any;
+
+    public options: Array < any > = this.options || [];
+    public selectedOptions: any[];
+    public filters: {
+        [s: string]: any;
+    } = {};
+    public column: string;
+    public activeColumn: string;
 
     constructor() {}
 
@@ -34,6 +44,53 @@ export class HeaderComponent {
             order = this.sortOrder;
         }
         return order;
+    }
+
+    showColumnMenu(event, column) {
+        //event.stopPropagation();
+        this.column = column;
+        this.selectedOptions = this.filters[this.column];
+
+        if (column === 'www') {
+            this.options = [
+                { id: 12, name: 'Option 12' },
+                { id: 2, name: 'Option 2' },
+                { id: 3, name: 'Option 3' },
+                { id: 4, name: 'Option 4' }
+            ];
+        } else {
+            this.options = [
+                { id: 1, name: 'Option 1' },
+                { id: 2, name: 'Option 2' },
+                { id: 3, name: 'Option 3' },
+                { id: 4, name: 'Option 4' },
+                { id: 5, name: 'Option 5' },
+                { id: 6, name: 'Option 6' },
+            ];
+        }
+        let el = event.target.parentNode;
+        let width = (el.offsetWidth > 200) ? el.offsetWidth : 200;
+        this.selectFilter.show(width, el.offsetHeight, el.offsetLeft);
+    }
+
+    isFilter(column): boolean {
+        let length = this.filters[column] && this.filters[column].length || 0;
+        return length > 0 ? true : false;
+    }
+
+    onChangeSelect(event) {
+        this.filters[this.column] = event;
+        this.selectedOptions = event;
+/*        console.log(this.selectedOptions);
+        console.log(this.filters);*/
+    }
+
+    onColumnMouseEnter(event, column) {
+        this.activeColumn = column;
+    }
+    
+    onColumnMouseLeave(event, column) {
+        this.activeColumn = null;
     }
 
 }
