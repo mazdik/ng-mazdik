5ea239bba4057d950c8a4f665d0ee70ef147aa56
User <remappp@gmail.com>
2017-03-19 18:04:28 +0500

    loading-indicator

--- loading-indicator/loading-indicator.component.ts
+++ loading-indicator/loading-indicator.component.ts
@@ -2,11 +2,11 @@ import { Component, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'loading-indicator',
-  template: `<div [style.visibility]="isLoading ? 'visible': 'hidden'" class="loader"></div>`,
+  template: `<div [style.visibility]="isLoading ? 'visible': 'hidden'" class="spinner"></div>`,
   styleUrls: ['./loading-indicator.css'],
 })
 export class LoadingIndicatorComponent {
-  @Input() public isLoading = false;
+  @Input() public isLoading: boolean = false;
 
   constructor() {
   }
