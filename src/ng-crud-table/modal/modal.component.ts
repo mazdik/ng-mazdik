4e166cff5a71a6b92b14d9d6e3910b14db796649
mazdik <remappp@gmail.com>
2017-11-17 22:14:42 +0500

    draggable modal

--- src/ng-crud-table/modal/modal.component.ts
+++ src/ng-crud-table/modal/modal.component.ts
@@ -14,6 +14,9 @@ export class ModalComponent implements OnInit, AfterViewChecked {
 
   public visible: boolean = false;
   executePostDisplayActions: boolean;
+  dragging: boolean;
+  lastPageX: number;
+  lastPageY: number;
 
   constructor() { }
 
@@ -38,6 +41,11 @@ export class ModalComponent implements OnInit, AfterViewChecked {
     this.hide();
   }
 
+  @HostListener('mousemove', ['$event'])
+  onMousemove(event): void {
+    this.onDrag(event);
+  }
+
   public show(): void {
     this.executePostDisplayActions = true;
     this.visible = true;
@@ -47,7 +55,7 @@ export class ModalComponent implements OnInit, AfterViewChecked {
   }
 
   public hide(): void {
-    setTimeout(() => this.visible = false, 300);
+    this.visible = false;
   }
 
   get contentzIndex(): number {
@@ -72,6 +80,33 @@ export class ModalComponent implements OnInit, AfterViewChecked {
 
     this.modalRoot.nativeElement.style.left = x + 'px';
     this.modalRoot.nativeElement.style.top = y + 'px';
+  }
+
+  initDrag(event: MouseEvent) {
+      this.dragging = true;
+      this.lastPageX = event.pageX;
+      this.lastPageY = event.pageY;
+      this.modalRoot.nativeElement.classList.add('dragging');
+  }
+
+  onDrag(event: MouseEvent) {
+    if (this.dragging) {
+      const deltaX = event.pageX - this.lastPageX;
+      const deltaY = event.pageY - this.lastPageY;
+      const leftPos = parseInt(this.modalRoot.nativeElement.style.left, 10);
+      const topPos = parseInt(this.modalRoot.nativeElement.style.top, 10);
+
+      this.modalRoot.nativeElement.style.left = leftPos + deltaX + 'px';
+      this.modalRoot.nativeElement.style.top = topPos + deltaY + 'px';
+
+      this.lastPageX = event.pageX;
+      this.lastPageY = event.pageY;
+    }
+  }
+
+  endDrag(event: MouseEvent) {
+      this.dragging = false;
+      this.modalRoot.nativeElement.classList.remove('dragging');
   }
 
 }
