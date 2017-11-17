015f39b355d5f6a32b209e4022d373101c594a0b
mazdik <remappp@gmail.com>
2017-11-17 18:39:23 +0500

    update modal

--- src/ng-crud-table/modal/modal.component.ts
+++ src/ng-crud-table/modal/modal.component.ts
@@ -1,36 +1,77 @@
-import {Component, ElementRef, ViewChild, Input} from '@angular/core';
+import {Component, ElementRef, ViewChild, Input, OnInit, AfterViewChecked, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: 'modal.component.html',
   styleUrls: ['modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit, AfterViewChecked {
 
   @Input() public modalTitle: string;
-
-  @ViewChild('modalRoot')
-  public modalRoot: ElementRef;
+  @Input() width: any;
+  @Input() zIndex: number = 900;
+  @ViewChild('modalRoot') modalRoot: ElementRef;
 
   public visible: boolean = false;
-  public visibleAnimate: boolean = false;
-  public static: boolean = false;
+  executePostDisplayActions: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewChecked() {
+    if (this.executePostDisplayActions) {
+      this.center();
+      this.executePostDisplayActions = false;
+    }
+  }
+
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.executePostDisplayActions = true;
+    this.center();
+  }
+
+  @HostListener('keydown.esc')
+  onKeyDown(): void {
+    this.hide();
+  }
 
   public show(): void {
+    this.executePostDisplayActions = true;
     this.visible = true;
     setTimeout(() => {
-      this.visibleAnimate = true;
       this.modalRoot.nativeElement.focus();
     }, 1);
   }
 
   public hide(): void {
-    this.visibleAnimate = false;
     setTimeout(() => this.visible = false, 300);
   }
 
-  public preventClosing(event: MouseEvent) {
-    event.stopPropagation();
+  get contentzIndex(): number {
+    return this.zIndex + 1;
+  }
+
+  center() {
+    let elementWidth = this.modalRoot.nativeElement.offsetWidth;
+    let elementHeight = this.modalRoot.nativeElement.offsetHeight;
+
+    if (elementWidth === 0 && elementHeight === 0) {
+      this.modalRoot.nativeElement.style.visibility = 'hidden';
+      this.modalRoot.nativeElement.style.display = 'block';
+      elementWidth = this.modalRoot.nativeElement.offsetWidth;
+      elementHeight = this.modalRoot.nativeElement.offsetHeight;
+      this.modalRoot.nativeElement.style.display = 'none';
+      this.modalRoot.nativeElement.style.visibility = 'visible';
+    }
+
+    const x = Math.max((window.innerWidth - elementWidth) / 2, 0);
+    const y = Math.max((window.innerHeight - elementHeight) / 2, 0);
+
+    this.modalRoot.nativeElement.style.left = x + 'px';
+    this.modalRoot.nativeElement.style.top = y + 'px';
   }
 
 }
