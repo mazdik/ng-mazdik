f5bbfedc5490dc18839d8d79b9b38f873db2cbc8
mazdik <remappp@gmail.com>
2018-01-26 19:10:19 +0500

    add class BaseFilter

---
+++ src/ng-crud-table/models/base-filter.ts
@@ -0,0 +1,114 @@
+import {Filter} from '../types';
+
+export class BaseFilter {
+
+  public static EQUALS = 'equals'; // =
+  public static NOT_EQUAL = 'notEqual'; // !=
+  public static LESS_THAN = 'lessThan'; // <
+  public static LESS_THAN_OR_EQUAL = 'lessThanOrEqual'; // <=
+  public static GREATER_THAN = 'greaterThan'; // >
+  public static GREATER_THAN_OR_EQUAL = 'greaterThanOrEqual'; // >=
+  public static IN_RANGE = 'inRange'; // in
+
+  public static CONTAINS = 'contains'; // like lower(%val%);
+  public static NOT_CONTAINS = 'notContains'; // not like lower(%val%);
+  public static STARTS_WITH = 'startsWith'; // like val%;
+  public static ENDS_WITH = 'endsWith'; // like %val;
+
+  filter(data: any[], filters: Filter) {
+    let filteredRows: any[] = data;
+    for (const key in filters) {
+      if (filters[key].value) {
+        filteredRows = filteredRows.filter((row: any) => {
+          if (key in row) {
+            return this.comparator(filters[key].matchMode, row[key], filters[key].value);
+          } else {
+            return false;
+          }
+        });
+      }
+    }
+    return filteredRows;
+  }
+
+  comparator(matchMode: string, value: any, filter: any) {
+    switch (matchMode) {
+      case BaseFilter.EQUALS:
+        return this.equals(value, filter);
+      case BaseFilter.NOT_EQUAL:
+        return this.notEquals(value, filter);
+      case BaseFilter.IN_RANGE:
+        return this.in(value, filter);
+      default:
+        return this.equals(value, filter);
+    }
+  }
+
+  equals(value, filter): boolean {
+    if (filter === undefined || filter === null || (typeof filter === 'string' && filter.trim() === '')) {
+      return true;
+    }
+    if (value === undefined || value === null) {
+      return false;
+    }
+    return value.toString().toLowerCase() === filter.toString().toLowerCase();
+  }
+
+  notEquals(value, filter): boolean {
+    if (filter === undefined || filter === null || (typeof filter === 'string' && filter.trim() === '')) {
+      return false;
+    }
+    if (value === undefined || value === null) {
+      return true;
+    }
+    return value.toString().toLowerCase() !== filter.toString().toLowerCase();
+  }
+
+  in(value, filter: any[]): boolean {
+    if (filter === undefined || filter === null || filter.length === 0) {
+      return true;
+    }
+    if (value === undefined || value === null) {
+      return false;
+    }
+    for (let i = 0; i < filter.length; i++) {
+      if (filter[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  startsWith(value, filter): boolean {
+    if (filter === undefined || filter === null || filter.trim() === '') {
+      return true;
+    }
+    if (value === undefined || value === null) {
+      return false;
+    }
+    const filterValue = filter.toLowerCase();
+    return value.toString().toLowerCase().slice(0, filterValue.length) === filterValue;
+  }
+
+  endsWith(value, filter): boolean {
+    if (filter === undefined || filter === null || filter.trim() === '') {
+      return true;
+    }
+    if (value === undefined || value === null) {
+      return false;
+    }
+    const filterValue = filter.toString().toLowerCase();
+    return value.toString().toLowerCase().indexOf(filterValue, value.toString().length - filterValue.length) !== -1;
+  }
+
+  contains(value, filter): boolean {
+    if (filter === undefined || filter === null || (typeof filter === 'string' && filter.trim() === '')) {
+      return true;
+    }
+    if (value === undefined || value === null) {
+      return false;
+    }
+    return value.toString().toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+  }
+
+}
