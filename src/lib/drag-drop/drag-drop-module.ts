11e3d734e16b46745b4c3761e0f4671cd3a05899
mazdik <remappp@gmail.com>
2019-02-05 14:22:06 +0500

    delete old drag and drop

--- src/lib/drag-drop/drag-drop-module.ts
+++ src/lib/drag-drop/drag-drop-module.ts
@@ -1,22 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DraggableModule} from '../draggable';
-import {DragObjectDirective} from './drag-object.directive';
 import {DropElementDirective} from './drop-element.directive';
 
 @NgModule({
   imports: [
     CommonModule,
-    DraggableModule,
   ],
   declarations: [
-    DragObjectDirective,
     DropElementDirective,
   ],
   exports: [
-    DragObjectDirective,
     DropElementDirective,
-  ],
-  providers: []
+  ]
 })
 export class DragDropModule {}
