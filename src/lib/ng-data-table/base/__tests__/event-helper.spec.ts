9611d9f389b141143fe0b86055ecafd637763dcb
mazdik <remappp@gmail.com>
2019-03-09 22:30:32 +0500

    test: event helper

---
+++ src/lib/ng-data-table/base/__tests__/event-helper.spec.ts
@@ -0,0 +1,48 @@
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import {EventHelper} from '../event-helper';
+
+@Component({
+  selector: 'test-fixture-component',
+  template: `
+  <div class="datatable-row">
+    <div class="datatable-body-cell" data-column-index="1" data-row-index="4">
+      <div class="dt-inline-data" (click)="clickEvent=$event"></div>
+    </div>
+  </div>
+  `
+})
+class TestFixtureComponent {
+  clickEvent: any;
+}
+
+describe('EventHelper', () => {
+  let fixture: ComponentFixture<TestFixtureComponent>;
+  let component: TestFixtureComponent;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestFixtureComponent]
+    });
+  });
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TestFixtureComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement;
+    });
+  }));
+
+  it('should be able to find cell event', () => {
+    const debugElement = fixture.debugElement.query(By.css('.dt-inline-data'));
+    debugElement.nativeElement.click();
+
+    const result = EventHelper.findCellEvent(component.clickEvent, element);
+    expect(result.columnIndex).toBe(1);
+    expect(result.rowIndex).toBe(4);
+  });
+});
