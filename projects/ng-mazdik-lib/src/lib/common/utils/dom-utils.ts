833e488588a1501868f43a1cfeebb00503b52721
mazdik <remappp@gmail.com>
2019-12-06 17:30:16 +0500

    fix drag and resize in mobile

--- projects/ng-mazdik-lib/src/lib/common/utils/dom-utils.ts
+++ projects/ng-mazdik-lib/src/lib/common/utils/dom-utils.ts
@@ -1,4 +1,7 @@
 export function isLeftButton(event: MouseEvent | TouchEvent) {
+  if (event.type === 'touchstart') {
+    return true;
+  }
   return (event.type === 'mousedown' && (event as MouseEvent).button === 0);
 }
 
